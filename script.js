4dd1dab1261b542f5abf43b04d492d74226495cb
isarupolo <isabelli.rupolo.borges@escola.pr.gov.br>
2026-04-29 22:33:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+onst avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
